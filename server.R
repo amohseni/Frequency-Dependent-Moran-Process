81e609666397fe40f4c84f3a2e501d48189404f1
Aydin <aydin.mohseni@gmail.com>
2015-09-27 17:24:38 -0700

    UI Updates
    added tabs to the main panel
    & many minor improvements.

--- server.R
+++ server.R
@@ -14,30 +14,47 @@ shinyServer(function(input, output, session) {
   output$stationaryDistribution <- renderPlot({
     
 # FINDING THE STATIONARY DISTRIBUION OF THE MORAN PROCESS   
-    # Let G be a 2x2 symmetric game 
+    # Let Γ be a 2x2 symmetric game 
     # with two players,
     # pure strategies A and B,
     # and payoff matrix M = matrix(c(a,b,c, d), nrow=2, ncol=2, byrow=TRUE).
 
     # Then the payoff to each combination of actions would be given by
     # U(A,A)=a, U(A,B)=b, U(B,A)=c, U(B,B)=d. 
+
+    # The five traditional game types are provided as defaults:
+    if (input$interaction_structure=='Anticoordination Game') {
+      updateNumericInput(session, "a", value =1)
+      updateNumericInput(session, "b", value =3)
+      updateNumericInput(session, "c", value =2)
+      updateNumericInput(session, "d", value =1)
+    }
+    if (input$interaction_structure=='Coordination Game') {
+      updateNumericInput(session, "a", value = 3)
+      updateNumericInput(session, "b", value = 0)
+      updateNumericInput(session, "c", value = 2)
+      updateNumericInput(session, "d", value = 2)
+    }
+    if (input$interaction_structure=='Dominating Strategy Game') {
+      updateNumericInput(session, "a", value = 3)
+      updateNumericInput(session, "b", value = 1)
+      updateNumericInput(session, "c", value = 2)
+      updateNumericInput(session, "d", value = 0)
+    }
+    if (input$interaction_structure=='Degenerate Game') {
+      updateNumericInput(session, "a", value = 1)
+      updateNumericInput(session, "b", value = 1)
+      updateNumericInput(session, "c", value = 1)
+      updateNumericInput(session, "d", value = 1)
+    }
+    if (input$interaction_structure=='Custom') {
+    }
     
     # The payoff values a, b, c, d are input from the user interface
     a <- as.numeric(input$a)
     b <- as.numeric(input$b)
     c <- as.numeric(input$c)
     d <- as.numeric(input$d)    
-
-    # The five traditional game types are provided as defaults:
-    # The Degenerate Game (0)
-    #assignVec(c('a','b', 'c', 'd'), c(1, 1, 1, 1), envir = .GlobalEnv)
-    # Dominating Strategy Game [e.g., Prisoner's Dilemma]
-    # assignVec(c('a','b', 'c', 'd'), c(2, 0, 3, 1), envir = .GlobalEnv)
-    # Coordination Game [e.g., Stag Hunt]
-    # assignVec(c('a','b', 'c', 'd'), c(3, 0, 2, 2), envir = .GlobalEnv)  
-    # Anticoordination Game [e.g., Hawk-Dove]
-    # assignVec(c('a','b', 'c', 'd'), c(0, 3, 1, 2), envir = .GlobalEnv)
-    #assignVec(c('a','b', 'c', 'd'), c(1, 3, 2, 1), envir = .GlobalEnv)
         
     # Population Size (we are interested in 'Large Populations' where N -> ∞)
     N <- as.numeric(input$populationSize)
@@ -60,15 +77,17 @@ shinyServer(function(input, output, session) {
     # Fitness (Reproductive/Imitative Success) of Each Strategy
     fA <- function(i) (1 - w + w * FA(i))
     fB <- function(i) (1 - w + w * FB(i))
-
-    # Mutation Term
-    µ <- as.numeric(input$mutationRate)
+    # Population Mean Fitness
+    P <- function(i) (i * fA(i) + (N-i) * fB(i))
+    
+    # Mutation Rate
+    η <- as.numeric(input$mutationRate)
     
     #Transition Proabilities for Moran Process with Mutation
     # Pr(j -> j+1)
-    PforwardM <- function(i) (1-µ)*((i * fA(i) / P(i)) * ((N-i) / N)) + µ*(((N-i) * fB(i) / P(i)) * ((N-i) / N))
+    PforwardM <- function(i) (1-η)*((i * fA(i) / P(i)) * ((N-i) / N)) + η*(((N-i) * fB(i) / P(i)) * ((N-i) / N))
     # Pr(j -> j-1)		
-    PbackM <- function(i) (1-µ)*(((N-i) * fB(i) / P(i)) * (i / N)) + µ*((i * fA(i) / P(i)) * (i / N))
+    PbackM <- function(i) (1-η)*(((N-i) * fB(i) / P(i)) * (i / N)) + η*((i * fA(i) / P(i)) * (i / N))
     # Pr(j -> j)		
     PstayM <- function(i) 1 - (PforwardM(i) + PbackM(i))
     
@@ -79,6 +98,7 @@ shinyServer(function(input, output, session) {
     rownames(MPM) = c(0:N)
     colnames(MPM) = c(0:N)
     
+    # We compute the transition matrix by recursive applications of the transition probabilities
     MPM <- outer(0:N, 0:N, FUN=function(r,c) 
       ifelse(c == r-1, PbackM(r),
              ifelse(c == r, PstayM(r), 
@@ -87,23 +107,22 @@ shinyServer(function(input, output, session) {
 
     # Denote MPM^∞ := MPMlim
     MPMlim <- MPM %^% 10000000
-
-    # We print the title of the stationary distribution
-    print("The Moran Process with Mutation Under the 2x2 Symmetric Game M")
-    # then print the stationary disribution π for the MPM (and round the entries to the N/2 digit):
-    π <- MPMlim[round(N/2),]
-    print("Stationary Distribution π of the MPM")
-    print(π)
+    # Select any row from the MPM^∞ matrix to get the stationary disribution µ for the MPM
+    µ <- MPMlim[round(N/2),]
+    # For testing purposes, print the stationary distribution µ
+    print("Stationary Distribution µ of the MPM")
+    print(µ)
     
     # Get the max (y-axis) probability value from the interace
     probMax <- as.numeric(input$probabilityMax)
 
-    # Draw the Barplot
-    barplot(π, col=rgb(82, 140, 202, max = 255), main="Stationary Distribution", ylab=expression('Probability µ'[i]), xlab="# of A-types in the population (i)", ylim=c(0,probMax), space=0)
-    # Establish the interval between x-axis marks as a function of N.
-    xInterval <- round(N/10, digits = 0)
+    # Draw the barplot
+    barplot(µ, col=rgb(82, 140, 202, max = 255), main="Stationary (Limit) Distribution", ylab=expression("Probability (µ'[i])"), xlab="# of A-types in the population (i)", ylim=c(0,probMax), space=0)
+    # The interval between ticks on the x-axis shiould be a function of N.
+    xInterval <- if (N>9) { round(N/10, digits = 0) } else { round(1+N/10, digits = 0) } # the else is to prevent the intervals from going to zero at small population sizes, and outputting an error
     axis(side=1, at=seq(0,N, by = xInterval))
-    box()
+    # Remove the box around the barplot
+    box() 
   })
 
   
@@ -111,43 +130,43 @@ shinyServer(function(input, output, session) {
   
 # SIMULATING THE MORAN PROCESS FOR A SINGLE POPULATION
 
-  # We create the function that simulates the population from a random initial condition
-
+  # We create the function that simulates the population from a random initial population composition
   output$singlePopulationSimulation <- renderPlot({
     simulationResetVariable<-input$simulateSinglePopulation
-    # Inputs: 
-    # Duration of simulation/number of generations of birth
-    t <- as.numeric(input$simulationTime)
-    # Population Size
-    N <- as.numeric(input$populationSize)
-    # Mutation Rate
-    µ <- as.numeric(input$mutationRate)    
-    # Intensity of Selection
-    w <- as.numeric(input$intensityOfSelection)    
-    
-    #Transition Proabilities for Moran Process with Mutation
-    # Pr(j -> j+1)
-    PforwardM <- function(i) (1-µ)*((i * fA(i) / P(i)) * ((N-i) / N)) + µ*(((N-i) * fB(i) / P(i)) * ((N-i) / N))
-    # Pr(j -> j-1)		
-    PbackM <- function(i) (1-µ)*(((N-i) * fB(i) / P(i)) * (i / N)) + µ*((i * fA(i) / P(i)) * (i / N))
-    # Pr(j -> j)		
-    PstayM <- function(i) 1 - (PforwardM(i) + PbackM(i))
-        
-    # The MPM Transition Matrix :: (MPM)
-    MPM <- matrix(nrow = N+1, ncol = N+1, byrow = TRUE)
 
-    # We relabel the row and columns to correspond to the number of A-types in the N-size population
-    rownames(MPM) = c(0:N)
-    colnames(MPM) = c(0:N)
-    
-    MPM <- outer(0:N, 0:N, FUN=function(r,c) 
-      ifelse(c == r-1, PbackM(r),
-             ifelse(c == r, PstayM(r), 
-                    ifelse(c == r+1, PforwardM(r),
-                           0))))
-    
-    # Output: A vector of length t
-    MPM.sim <- function(t, MPM, πNought) {
+    # INPUTS
+      # The payoffs a, b, c, d, from the game
+      a <- as.numeric(input$a)
+      b <- as.numeric(input$b)
+      c <- as.numeric(input$c)
+      d <- as.numeric(input$d) 
+      
+      # Duration of simulation/number of generations of birth & death
+      t <- as.numeric(input$simulationTime)
+      
+      # Population Size
+      N <- as.numeric(input$populationSize)
+      # Intensity of Selection
+      w <- as.numeric(input$intensityOfSelection)
+      # Mutation Rate
+      η <- as.numeric(input$mutationRate)
+
+      # The MPM Transition Matrix :: (MPM)
+      MPM <- matrix(nrow = N+1, ncol = N+1, byrow = TRUE)
+      
+      # We relabel the row and columns to correspond to the number of A-types in the N-size population
+      rownames(MPM) = c(0:N)
+      colnames(MPM) = c(0:N)
+      
+      # We compute the transition matrix by recursive applications of the transition probabilities
+      MPM <- outer(0:N, 0:N, FUN=function(r,c) 
+        ifelse(c == r-1, PbackM(r),
+               ifelse(c == r, PstayM(r), 
+                      ifelse(c == r+1, PforwardM(r),
+                             0))))
+      
+    # Simulation Output: A vector of length t
+    MPM.sim <- function(t, MPM, πNought) { # The simulation function takes as arguments: duration, the MPM, and some initial condition πNought
       sim <- as.numeric(t)
       if (missing(πNought)) { # If the initial state is not specified
         sim[1] <- (sample(1:(N+1),1) - 1) # Then take a random initial state 
--- ui.R
+++ ui.R
@@ -3,81 +3,118 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
+  # Title
   titlePanel("The Moran Process for 2x2 Symmetric Games"),
   
-  # Sidebar with a slider input for Population Size
+  # Sidebar for Parameter Input
   sidebarLayout(
-    sidebarPanel(
       
-    strong("Payoff / Fitness Matrix:"),
-      fluidRow(
-        column(6,
-               numericInput("a", 
-                   label = NULL, 
-                   value = 1,
-                   min = 0)
-        ),
-        column(6,
-               numericInput("b", 
-                   label = NULL, 
-                   value = 3,
-                   min = 0)
-        )),
+      sidebarPanel(
+    
+        selectInput("interaction_structure", "Game Type",
+            c("Degenerate Game", "Dominating Strategy Game", "Coordination Game", "Anticoordination Game", "Custom (to manually change payoffs)"),
+            selected = "Anticoordination Game"
+          ),
+        
+        uiOutput("ui"),
+        
+        # 2X2 Game Payoff Input Fields
+        strong("Payoff / Fitness Matrix:"),
+        
+          # 1st Row: Types
+          fluidRow(
+            column(2),
+            column(5,
+                   p("Type A", align="center")
+                   ),
+            column(5,
+                   p("Type B", align="center")
+            )),
 
-      fluidRow(
-        column(6,      
-              numericInput("c", 
-                   label = NULL, 
-                   value = 2,
-                   min = 0)
-        ), 
-        column(6,
-              numericInput("d", 
-                   label = NULL, 
-                   value = 1,
-                   min = 0)
-        )),
-      
-      sliderInput("populationSize",
-                  "Population Size:",
-                  min = 10,
-                  max = 100,
-                  value = 100),
-
+          # 2nd Row: Payoffs to Type A
+          fluidRow(
+            column(2,
+                   p("Type A", align="center")
+                   ),
+            column(5,
+                   
+                   numericInput("a", 
+                                label = NULL, 
+                                value = 1,
+                                min = 1)
+          ),
+          column(5,
+                 numericInput("b", 
+                              label = NULL, 
+                              value = 3,
+                              min = 1)
+          )),
+        
+          # 3rd Row: Payoffs to Type B
+          fluidRow(
+            column(2,
+                   p("Type B", align="center")
+                  ),
+            column(5,      
+                   numericInput("c", 
+                                label = NULL, 
+                                value = 2,
+                                min = 1)
+            ), 
+            column(5,
+                   numericInput("d", 
+                                label = NULL, 
+                                value = 1,
+                                min = 1)
+            )),
+        
+        # Population Size Slider
+        sliderInput("populationSize",
+                    "Population Size:",
+                    min = 2,
+                    max = 200,
+                    value = 100),
+        
+        # Mutation Rate Slider
         sliderInput("mutationRate",
                     "Mutation Rate:",
                     min = 0,
                     max = .1,
-                    value = .05),
-
-        sliderInput("intensityOfSelection",
-                      "Intensity of Selection:",
-                      min = 0,
-                      max = 1,
-                      value = .1),
+                    value = .02),
         
+        # Intensity of Selection Slider        
+        sliderInput("intensityOfSelection",
+                    "Intensity of Selection:",
+                    min = 0,
+                    max = 1,
+                    value = .1),
+
+        # Display Max Probability (Y-Axis)                         
         sliderInput("probabilityMax",
-                    "Display Max Probability:",
+                    "(Y-Axis) Display Max Probability:",
                     min = 0,
                     max = 1,
                     value = .05),
-    
+        
         tags$head(tags$script(src = "message-handler.js")),
         p(actionButton("simulateSinglePopulation", "Simulate Single Population"), align = "center"),
-    
+        
+        # Simulation Time Slider
         sliderInput("simulationTime",
                     "Simulation Time:",
                     min = 1,
                     max = 10000,
                     value = 1000)
-    
-        ),
-    
-    # Show a plot of the generated distribution
+      ),
+  
+  # Main Panel with Stationary Distribution + Simulation & Stats Panels
     mainPanel(
-      plotOutput("stationaryDistribution"),
-      plotOutput("singlePopulationSimulation")
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Stationary (Limit) Distribution & Simulation", 
+                           plotOutput("stationaryDistribution"), 
+                           plotOutput("singlePopulationSimulation")),
+                  tabPanel("Replacement Probabilities & Invasion Dynamics", verbatimTextOutput("summary")) 
+      )
     )
   )
 ))
